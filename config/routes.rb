8a8559cbb1ae085800aed5b2b1808c2dfb0c2d50
Mihaela Sava <mihaela.sava@eventya.net>
2025-10-27 21:56:02 +0200

    Fix Rails routing and config, remove static pages, add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  # Ruta pentru health check (folosită de Kamal)
-  get "up", to: "rails/health#show", as: :up
-
-  # Ruta principală a site-ului tău
   root "home#index"
 end
